09633bd2623147749445bca7d6bccd58db64552e
Dylan Stocking <dstocking0823@gmail.com>
2025-10-06 17:49:11 -0400

    Move all files up a level

---
+++ src/App.vue
@@ -0,0 +1,27 @@
+<script setup lang="ts">
+import TheHeader from './components/TheHeader.vue'
+import TheFooter from './components/TheFooter.vue'
+import HomePage from './components/HomePage.vue'
+</script>
+
+<template>
+  <div class="app-container">
+    <TheHeader />
+    <main>
+      <HomePage />
+    </main>
+    <TheFooter />
+  </div>
+</template>
+
+<style scoped>
+.app-container {
+  display: flex;
+  flex-direction: column;
+  min-height: 100vh;
+}
+
+main {
+  flex: 1;
+}
+</style>
